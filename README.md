7795049ad4bad46cb2e8f86c2be37c34037bef3a
David Bak <dbak.db@gmail.com>
2024-08-09 14:26:17 -0400

    update weather forecast script

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# python-api-scripts
+# Python API Scripts
+
+Here are some basic scripts that interact with some popular APIs.
--- weather_forecast.py
+++ weather_forecast.py
@@ -1,33 +1,56 @@
-import json
 import requests
 import os
-import sys
 import argparse
 
-# Parse the input arguments
-parser = argparse.ArgumentParser(
-                    prog='WeatherForecaster',
+def kel_to_cel(kel):
+    return kel - 273.15
+
+def get_weather(api_key, lat, lon):
+    """
+    Given the latitude and longitude, return the current weather.
+    """
+    excludes = "alerts,daily,hourly,minutely"
+    api_url = f"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={excludes}&appid={api_key}"
+    response = requests.get(api_url)
+
+    weather_info = response.json()["current"]
+    current_temp = kel_to_cel(weather_info["temp"])
+    print(f"The current temperature is {current_temp}")
+
+def get_coordinates(api_key, city, state, country_code):
+    """
+    Given the city, state/region, and country code, find the coordinates.
+    """
+    api_url = f"http://api.openweathermap.org/geo/1.0/direct?q={city},{state},{country_code}&appid={api_key}"
+    response = requests.get(api_url)
+    city_obj = response.json()[0]
+
+    return city_obj["lat"], city_obj["lon"]
+
+def get_api_key():
+    """
+    Check the environment for the API key.
+    """
+    return os.getenv("WEATHER_API_KEY", default=None)
+
+def main():
+    # Parse the input arguments
+    parser = argparse.ArgumentParser(
+                    prog='weather_forecast.py',
                     description='Provides the weather in any given city')
 
-parser.add_argument('-c', '--city')
-parser.add_argument('-s', '--state')
-parser.add_argument('-n', '--country')
+    parser.add_argument('-c', '--city')
+    parser.add_argument('-s', '--state')
+    parser.add_argument('-n', '--country')
 
-args = parser.parse_args()
-print(args)
+    args = parser.parse_args()
+    location = vars(args)
+    api_key = get_api_key()
 
+    if api_key == None:
+        print("The WEATHER_API_KEY is not set.")
+    else:
+        lat, lon = get_coordinates(api_key, location["city"], location["state"], location["country"])
+        get_weather(api_key, lat, lon)
 
-# def get_weather(lat, lon):
-
-# def get_coordinates(city, state, country_code):
-
-#     # return lat, lon
-
-# def get_api_key():
-
-
-
-
-    
-
-
+main()
